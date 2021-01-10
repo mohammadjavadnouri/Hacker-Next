0d27c799b8ed81c3909ca9f52599ca53423b86ac
Your Name <you@example.com>
2021-01-10 09:05:34 +0330

    fetch next page stories

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,60 @@
 const react = require("react");
-import fetch from 'isomorphic-fetch'
-import Error from 'next/error';
-import StoryList from '../components/StoryList';
-import Layout from '../components/Layout';
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import Link from "next/link";
+import StoryList from "../components/StoryList";
+import Layout from "../components/Layout";
 
 class Index extends react.Component {
-    
-    static async getInitialProps() {
-        let stories;
-        try{
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
-            stories = await response.json();
-        } catch (err) {
-            console.log(err);
-            stories = [];
-        }
-
-        return {stories}
+  static async getInitialProps({ req, res, query }) {
+    let stories;
+    let page;
+    try {
+      page = Number(query.page) || 1;
+      const response = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
+      stories = await response.json();
+    } catch (err) {
+      console.log(err);
+      stories = [];
     }
 
-    render(){
-        const {stories} = this.props;
+    return { stories, page };
+  }
 
-        if(stories.length === 0){
-            return(
-                <Error statusCode={503}/>
-            )
-        }
+  render() {
+    const { stories, page } = this.props;
 
-        return(
-            <Layout title="Hacker Next" description="A Hacker News Clone made with Nextjs">
-                <StoryList stories={stories}  />
-            </Layout>
-        )
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
     }
+
+    return (
+      <Layout
+        title="Hacker Next"
+        description="A Hacker News Clone made with Nextjs"
+      >
+        <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
 }
-
 
 export default Index;
